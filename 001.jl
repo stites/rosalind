4094ea9bafc39b2442efa3ceca4e2bff2c0657f5
Sam Stites <sam@stites.io>
2014-07-20 23:53:30 -0700

    add - while-loop

--- 001.jl
+++ 001.jl
@@ -33,7 +33,24 @@ if n % 2 == 0
   return false
 end
 
+i = 3
+while i <=sqrt(n)
+  if n % i == 0
+    return false
+  end
+  i += 2
+end
+
 return true
 end
 
-println(is_prime(100))
+println(is_prime(1))
+println(is_prime(2))
+println(is_prime(3))
+println(is_prime(4))
+println(is_prime(5))
+println(is_prime(6))
+println(is_prime(7))
+println(is_prime(8))
+println(is_prime(9))
+println(is_prime(10))
